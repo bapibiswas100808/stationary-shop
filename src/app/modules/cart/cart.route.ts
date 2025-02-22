f8fdc7c12059235ccb3bef26150075c49905eacb
bapibiswas100808 <star.bappy@gmail.com>
2025-02-23 03:22:57 +0600

    updated

--- src/app/modules/cart/cart.route.ts
+++ src/app/modules/cart/cart.route.ts
@@ -5,10 +5,14 @@ import { USER_ROLE } from '../users/user.constant';
 
 const router = express.Router();
 
-router.get('/', auth(USER_ROLE.user), CartControllers.getCartByUserEmail);
+router.get(
+  '/',
+  auth(USER_ROLE.user, USER_ROLE.admin),
+  CartControllers.getCartByUserEmail,
+);
 router.post(
   '/addToCart',
-  auth(USER_ROLE.user),
+  auth(USER_ROLE.user, USER_ROLE.admin),
   CartControllers.addProductToCart,
 );
 
